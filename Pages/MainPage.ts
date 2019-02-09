b8b2bb7ef1c6f5c977202e65dd6c23f553a77a26
keltan <amiklasewicz@gmail.com>
2019-02-09 14:30:50 +0100

    Added login to amazon test

--- Pages/MainPage.ts
+++ Pages/MainPage.ts
@@ -4,18 +4,21 @@ import LoginPage from './LoginPage';
 
 
 export default class MainPage {
+    
+    private loginHover: Selector;
     private loginPageSelector: Selector;
+    
 
 
     public constructor () {
-
-        this.loginPageSelector = Selector('span.nav-action-inner');
+        
+        this.loginPageSelector = Selector('span.nav-line-2').withText('Account & Lists');
 
     }
 
     async goToLoginPage() {
         await t.click(this.loginPageSelector);
-        return new LoginPage();
+         return new LoginPage();
     }
 
 }
--- Tests/login.test.ts
+++ Tests/login.test.ts
@@ -2,9 +2,10 @@ import {Selector} from 'testcafe';
 import MainPage from '../Pages/MainPage';
 import LoginPage from '../Pages/LoginPage';
 import { amazonURL } from '../Helpers/PageUrlHelper';
+require('dotenv').config();
 
 fixture('login user')
-.page(amazonURL());
+.page('https://www.amazon.com/');
 
 test('login to amazon', async () =>{
     const mainPage = new MainPage();
